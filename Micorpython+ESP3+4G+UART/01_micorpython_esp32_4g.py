7e50bb9aff92b00f0cc0c243e2d22b15669c9c99
Hery <thitphavanh.psv@gmail.com>
2022-08-06 20:48:49 +0700

    update

--- Micorpython+ESP3+4G+UART/01_micorpython_esp32_4g.py
+++ Micorpython+ESP3+4G+UART/01_micorpython_esp32_4g.py
@@ -1,8 +1,7 @@
 from machine import UART
 
 uart = UART(2, 115200)
-uart.write(
-    "config,set,tcp,1,ttluart,1,1,hello,60,115.28.208.190,8080,0,0,0,0,0,0\r\n")
+uart.write("config,set,tcp,1,ttluart,1,1,hello,60,115.28.208.190,8080,0,0,0,0,0,0\r\n")
 
 uart.read()
 uart.write("config,set,save\r\n")
